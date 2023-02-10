2bd9ba1b6fd7eb55f04837be0351371ba6f508dd
mushfiq-rahman <mushfiq.style@gmail.com>
2023-02-11 01:11:40 +0600

    menu error fixed

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -17,7 +17,7 @@ const Sidenav = () => {
     <div>
       <AiOutlineMenu
         onClick={handleNav}
-        className="absolute top-4 right-4 z-[99] md:hidden"
+        className="fixed top-4 right-4 z-[99] md:hidden"
       />
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
@@ -87,12 +87,12 @@ const Sidenav = () => {
             <AiOutlineProject size={20} />
           </a>
 
-          <a
+          <a 
             href="#resume"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <BsPerson size={20} />
-          </a> onClick={handleNav}
+          </a> 
 
           <a
             href="#contact"
